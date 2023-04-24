9fa86bf634eac2c6ab5146ceffe48de39f94287b
dnapolitanno <dudadnapolitano@gmail.com>
2023-04-24 18:47:45 -0300

    feat: create config to project

--- src/entities/movies.entity.ts
+++ src/entities/movies.entity.ts
@@ -1,3 +1,17 @@
-class Movie {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+class Movie {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+  @Column({ type: "varchar", length: "50", unique: true })
+  name: string;
+  @Column({ nullable: true, type: "text" })
+  description?: Text | null | undefined;
+  @Column({ type: "integer" })
+  duration: number;
+  @Column({ type: "integer" })
+  price: number;
+}
 
 export default Movie;
